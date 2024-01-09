f61ce4be396a5b69ffa889f5994bacb17b5d444f
lihang <541573560@qq.com>
2024-01-09 16:59:05 +0800

    让服务正确的启动

--- app/xgb/xgb.py
+++ app/xgb/xgb.py
@@ -1,5 +1,6 @@
 '''
-xxxx
+xgb.py
+使用xgb模型进行预测
 '''
 from fastapi import APIRouter
 from typing import List
@@ -11,17 +12,18 @@ router = APIRouter()
 
 
 @router.post("/")
-async def read_root(data: List[int]) -> list:
+async def read_root(data: List) -> list:
     '''
     xxxx
     '''
 
     loaded_model = XGBClassifier()
-    loaded_model.load_model('../../resource/xgboost_model_35_67.json')
+    loaded_model.load_model('./resource/xgboost_model_35_67.json')
 
     np_data = np.array(data)
+    np_data = np_data.reshape(1, -1)
     y_pred = loaded_model.predict(np_data)
-    encoder = joblib.load('../../resource/label_encoder.joblib')
+    encoder = joblib.load('./resource/label_encoder.joblib')
     predictions = encoder.inverse_transform([round(value) for value in y_pred])
 
-    return predictions
+    return predictions.tolist()
--- cmd/mouse/mouse.py
+++ cmd/mouse/mouse.py
@@ -1,9 +1,14 @@
 """
-xxx
+模型服务启动文件
 """
 from fastapi import FastAPI
-from app.xgb import router as xgb_router
+from app.xgb.xgb import router as xgb_router
 
 app = FastAPI()
+
+# 探活接口
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 app.include_router(xgb_router, prefix="/xgb", tags=["xgb"])

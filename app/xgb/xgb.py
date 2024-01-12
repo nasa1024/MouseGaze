76990371c4f05cbae165baf77c1a7f839ed8b36a
lihang <541573560@qq.com>
2024-01-12 11:01:31 +0800

    文件依赖和模型位置修复

--- app/xgb/xgb.py
+++ app/xgb/xgb.py
@@ -18,12 +18,12 @@ async def read_root(data: List) -> list:
     '''
 
     loaded_model = XGBClassifier()
-    loaded_model.load_model('./resource/xgboost_model_35_67.json')
+    loaded_model.load_model('/app/resource/xgboost_model_35_67.json')
 
     np_data = np.array(data)
     np_data = np_data.reshape(1, -1)
     y_pred = loaded_model.predict(np_data)
-    encoder = joblib.load('./resource/label_encoder.joblib')
+    encoder = joblib.load('/app/resource/label_encoder.joblib')
     predictions = encoder.inverse_transform([round(value) for value in y_pred])
 
     return predictions.tolist()

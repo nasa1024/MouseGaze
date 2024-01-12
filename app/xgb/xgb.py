f7c6dde4ca2475baa4ccf21e89fd782cbe0e9732
lihang <541573560@qq.com>
2024-01-12 14:05:46 +0800

    修改项目依赖

--- app/xgb/xgb.py
+++ app/xgb/xgb.py
@@ -11,19 +11,19 @@ import numpy as np
 router = APIRouter()
 
 
-@router.post("/")
-async def read_root(data: List) -> list:
+@router.post("/predict")
+async def read_root(data: List[float]) -> list:
     '''
     xxxx
     '''
 
     loaded_model = XGBClassifier()
-    loaded_model.load_model('/app/resource/xgboost_model_35_67.json')
+    loaded_model.load_model('./resource/xgboost_model_35_67.json')
 
     np_data = np.array(data)
     np_data = np_data.reshape(1, -1)
     y_pred = loaded_model.predict(np_data)
-    encoder = joblib.load('/app/resource/label_encoder.joblib')
+    encoder = joblib.load('./resource/label_encoder.joblib')
     predictions = encoder.inverse_transform([round(value) for value in y_pred])
 
     return predictions.tolist()

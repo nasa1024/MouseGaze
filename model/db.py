519d315d127b9e1a876a5c161fadb67664d0ef6a
lihang <541573560@qq.com>
2024-01-12 18:42:28 +0800

    修复数据库连接问题

--- model/db.py
+++ model/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .spot import Base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mousegaze666@localhost:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mousegaze666@mypostgresdb:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

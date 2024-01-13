4c92481fb57bdea6e285cb8885d5a7695250ed63
lihang <541573560@qq.com>
2024-01-13 10:38:54 +0800

    修复异常情况

--- app/gaze/gaze.py
+++ app/gaze/gaze.py
@@ -17,8 +17,10 @@ async def price(data: List[int]) -> list:
     获取价格
     '''
 
+    
     # 查询数据库的数据
-    record = get_btcusdt_in_time_range(get_db, data[0], data[1])
+    with get_db() as db:
+        record = get_btcusdt_in_time_range(db, data[0], data[1])
     return {"data": record}
 
 
@@ -34,23 +36,24 @@ async def markers(data: List[float]) -> list:
     data = get_klines_from_time('BTCUSDT', '15m', before_1020_min, now)
     # 判断数据库是否有数据
     for i in data:
+        with get_db() as db:
         # 判断数据库是否有数据
-        if get_btcusdt_in_time_range(get_db, i[0], i[6]):
-            continue
-        # 插入数据
-        insert_btcusdt(get_db, {
-            'open_time': i[0],
-            'open': i[1],
-            'high': i[2],
-            'low': i[3],
-            'close': i[4],
-            'volume': i[5],
-            'close_time': i[6],
-            'quote_asset_volume': i[7],
-            'number_of_trades': i[8],
-            'taker_buy_base_asset_volume': i[9],
-            'taker_buy_quote_asset_volume': i[10]
-        })
+            if get_btcusdt_in_time_range(db, i[0], i[6]):
+                continue
+            # 插入数据
+            insert_btcusdt(get_db, {
+                'open_time': i[0],
+                'open': i[1],
+                'high': i[2],
+                'low': i[3],
+                'close': i[4],
+                'volume': i[5],
+                'close_time': i[6],
+                'quote_asset_volume': i[7],
+                'number_of_trades': i[8],
+                'taker_buy_base_asset_volume': i[9],
+                'taker_buy_quote_asset_volume': i[10]
+            })
 
     df = pd.DataFrame(data, columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time',
                       'quote_asset_volume', 'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore'])
